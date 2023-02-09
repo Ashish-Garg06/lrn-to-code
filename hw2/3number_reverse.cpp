25a1a36ea43fe671a9b51740a9736b8a9b236f60
Ashish Garg <ashishgarg5006@gmail.com>
2023-02-09 18:35:23 +0530

    added message and condition for single digit no

--- hw2/3number_reverse.cpp
+++ hw2/3number_reverse.cpp
@@ -7,17 +7,26 @@ int main()
     int inpnum,reversed_no=0;
     cout<<"Enter the number : ";
     cin>>inpnum;
-    int length_of_Num;
-
-    // couldn't find suitable function for int length so used log to calculate it
-    length_of_Num=(log10(inpnum)+1);
-
-    int divisor=inpnum;
-    for (int j = length_of_Num-1; j >= 0; j--)
+    // when user enters a single digit no
+    if (inpnum >= 0 && inpnum <= 9)
     {
-        reversed_no= (reversed_no+((divisor%10)*(pow(10,j))));
-        divisor=divisor/10;
+        cout << "Wow, what a thrilling and unique number you've entered. I'm sure the reverse of " << inpnum << " will blow your mind. it is drumroll please "<<inpnum <<endl;
     }
-    cout<<reversed_no;
+    
+    else
+    {
+        int length_of_Num;
+
+        // couldn't find suitable function for int length so used log to calculate it
+        length_of_Num=(log10(inpnum)+1);
+
+        int divisor=inpnum;
+        for (int j = length_of_Num-1; j >= 0; j--)
+        {
+            reversed_no= (reversed_no+((divisor%10)*(pow(10,j))));
+            divisor=divisor/10;
+        }
+        cout<<"you entered '"<<inpnum<<"' reverse of this is '"<<reversed_no<<"' \n";
+    }
     return 0;
 }
