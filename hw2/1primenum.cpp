32093fd1441927851bc7231cd8492a783c39a4f6
Ashish Garg <ashishgarg5006@gmail.com>
2023-03-22 19:09:50 +0530

    edit

--- hw2/1primenum.cpp
+++ hw2/1primenum.cpp
@@ -5,22 +5,23 @@ using namespace std;
 int main()
 {
     int inpnum;
-    cout<<"Enter the number you want to check : ";
-    cin>> inpnum;
+    cout << "Enter the number you want to check : ";
+    cin >> inpnum;
 
     // started from 2 becuz 0,1 divide every number; loop continue till inpnum-1 bcuz inpnum will divide itself
-    for (int i = 2; i <= (inpnum-1); i++)
+    for (int i = 2; i <= (inpnum - 1); i++)
     {
-        if ((inpnum % i)==0)
+        if ((inpnum % i) == 0)
         {
-            cout<<inpnum<<" is not a prime num as it is divisible by "<<i<<"\n";
+            cout << inpnum << " is not a prime num as it is divisible by " << i << "\n";
             break;
         }
-        else if (i==(inpnum-1))
+        else if (i == (inpnum - 1))
         {
-            cout<<inpnum<< " is a prime number"<<"\n";
+            cout << inpnum << " is a prime number"
+                 << "\n";
         }
-        
     }
+
     return 0;
 }
